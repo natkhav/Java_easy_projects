765728d2ac83b98a86064ca22904710a7061bb5e
Natalia Khavlina <natalia.khavlina@gmail.com>
2021-02-18 15:09:02 -0500

    Added Diamond Problem example

---
+++ DiamondProblem/src/diamond/First.java
@@ -0,0 +1,39 @@
+package diamond;
+
+interface First {
+	
+	default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}
+
+interface Third 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Third"); 
+    } 
+}  
+
+interface Fourth 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Fourth"); 
+    } 
+} 
+
+
+	
+	
+
+
